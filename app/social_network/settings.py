8ee3b53a7234e41d360130bac6f4c32441da3287
edil295 <edil.2905@gmail.com>
2023-01-20 10:46:45 +0600

    gires

--- app/social_network/settings.py
+++ app/social_network/settings.py
@@ -68,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.cont'
+                'ext_processors.messages',
             ],
         },
     },
@@ -125,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates', 'build', 'static')]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
